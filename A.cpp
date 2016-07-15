6a72587802c69f5337c23b28f3433247dab5a023
Daniel Saad Nogueira Nunes <daniel.saad.nunes@gmail.com>
2016-07-15 19:57:04 -0300

    saad: Kata

---
+++ A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	std::ios::sync_with_stdio(false);
+	int n;
+	while(cin>> n && n){
+		for(int j=0;j<n;j++){
+			double xi,yi;
+			cin >> xi >> yi;
+		}
+	}	
+	return 0;
+}
