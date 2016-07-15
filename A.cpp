72c4d86a5097673012479f58710017b97692cf33
Daniel Saad Nogueira Nunes <daniel.saad.nunes@gmail.com>
2016-07-15 20:02:59 -0300

    halian: inicio da função de calculo do raio da porra

--- A.cpp
+++ A.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
+
+int calculate_points(double x,double y){
+	double r = sqrt(x*x + y*y);
+	
+}
 
 int main(){
 	std::ios::sync_with_stdio(false);
 	int n;
 	while(cin>> n && n){
+		int points = 0;
 		for(int j=0;j<n;j++){
 			double xi,yi;
 			cin >> xi >> yi;
-		}
+			}
+
 	}	
 	return 0;
 }
