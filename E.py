6e86c56bd822811abb945ad4897fb435c23ce526
Daniel Saad Nogueira Nunes <daniel.saad.nunes@gmail.com>
2016-07-15 21:46:56 -0300

    meteficha: solution + golf

--- E.py
+++ E.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python
-
-def process_line(line):
-  	if eval(line.replace('=','==')):
-  		print 'Acertou miseravi'
-  	else:
-  		print 'Errou miseravi'
-
-if __name__ == '__main__':
-	n = input()
-    process_line(n)
+for _ in xrange(input()):
+	print 'Acertou' if eval(raw_input().replace('=','==')) else 'Errou', 'Miseravi!'
